ef746b06851584b8c883455ee3f6a0bf73fc5a48
Defn Nghiem <iam@defn.sh>
2017-08-11 10:34:37 -0700

    wrapper

---
+++ .wrapper.tf
@@ -0,0 +1,18 @@
+module "env" {
+  source = "module/fogg/env"
+
+  global_bucket = "${var.remote_bucket}"
+  global_key    = "${join("_",slice(split("_",var.remote_path),0,1))}/terraform.tfstate"
+  global_region = "${var.remote_region}"
+}
+
+data "terraform_remote_state" "org" {
+  backend = "s3"
+
+  config {
+    bucket         = "${var.remote_bucket}"
+    key            = "${join("_",slice(split("_",var.remote_path),0,1))}/terraform.tfstate"
+    region         = "${var.remote_region}"
+    dynamodb_table = "terraform_state_lock"
+  }
+}
