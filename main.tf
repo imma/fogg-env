eb5a7b11ef4344f0883715f5ddd8153c744ed10b
Defn Nghiem <iam@defn.sh>
2017-08-13 00:24:20 -0700

    need dns support

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ data "aws_partition" "current" {}
 
 resource "aws_vpc" "env" {
   cidr_block                       = "${data.external.org.result["cidr_${var.env_name}"]}"
-  enable_dns_support               = false
+  enable_dns_support               = true
   enable_dns_hostnames             = false
   assign_generated_ipv6_cidr_block = true
 
