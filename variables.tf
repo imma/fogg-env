a17b1690203eaf036e12db340669b369c9c3615a
Defn Nghiem <iam@defn.sh>
2017-08-23 07:15:34 +0000

    save execution arn for lambda permission

--- variables.tf
+++ variables.tf
@@ -207,3 +207,7 @@ output "api_gateway" {
 output "api_gateway_resource" {
   value = "${aws_api_gateway_rest_api.env.root_resource_id}"
 }
+
+output "api_gateway_execution_arn" {
+  value = "${aws_api_gateway_deployment.env.execution_arn}"
+}
