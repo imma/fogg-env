42c11a7d62a873ec231629f89c9ff113f0e5f9ab
Defn Nghiem <iam@defn.sh>
2017-08-25 20:14:26 -0700

    allow irc

--- nat.tf
+++ nat.tf
@@ -54,3 +54,12 @@ resource "aws_security_group_rule" "forward_allow_https" {
   source_security_group_id = "${aws_security_group.env.id}"
   security_group_id        = "${module.nat.network_sg}"
 }
+
+resource "aws_security_group_rule" "forward_irc" {
+  type                     = "ingress"
+  from_port                = 6667
+  to_port                  = 6667
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.env.id}"
+  security_group_id        = "${module.nat.network_sg}"
+}
