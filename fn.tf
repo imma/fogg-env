b93f8452b0883b1411a15ef29546ce7975e2f6e5
Defn Nghiem <iam@defn.sh>
2017-09-02 01:02:26 -0700

    base mapping depends on a deployment

--- fn.tf
+++ fn.tf
@@ -18,6 +18,39 @@ resource "aws_iam_role" "fn" {
   assume_role_policy = "${data.aws_iam_policy_document.fn.json}"
 }
 
+resource "aws_api_gateway_rest_api" "env" {
+  name = "${var.env_name}"
+}
+
+resource "aws_api_gateway_domain_name" "env" {
+  domain_name     = "${aws_route53_zone.private.name}"
+  certificate_arn = "${data.terraform_remote_state.org.wildcard_cert}"
+}
+
+resource "aws_route53_record" "env_api_gateway" {
+  zone_id = "${data.terraform_remote_state.org.public_zone_id}"
+  name    = "${aws_route53_zone.private.name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_api_gateway_domain_name.env.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.env.cloudfront_zone_id}"
+    evaluate_target_health = "true"
+  }
+}
+
+resource "aws_route53_record" "env_api_gateway_private" {
+  zone_id = "${aws_route53_zone.private.zone_id}"
+  name    = "${aws_route53_zone.private.name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_api_gateway_domain_name.env.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.env.cloudfront_zone_id}"
+    evaluate_target_health = "true"
+  }
+}
+
 module "fn" {
   source         = "git@github.com:imma/fogg-api-gateway//module/fn"
   function_name  = "${var.env_name}-fn"
@@ -36,6 +69,13 @@ module "env" {
 
   rest_api_id = "${aws_api_gateway_rest_api.env.id}"
   resource_id = "${aws_api_gateway_rest_api.env.root_resource_id}"
+}
+
+resource "aws_api_gateway_base_path_mapping" "env" {
+  depends_on  = ["aws_api_gateway_deployment.env"]
+  api_id      = "${aws_api_gateway_rest_api.env.id}"
+  stage_name  = "live"
+  domain_name = "${signum(length(var.env_zone)) == 1 ? var.env_zone : var.env_name}.${signum(length(var.env_domain_name)) == 1 ? var.env_domain_name : data.terraform_remote_state.org.domain_name}"
 }
 
 resource "aws_api_gateway_deployment" "env" {
