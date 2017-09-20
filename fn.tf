22215bf3acc1c999931af5c4e13fcae51c359382
Defn Nghiem <iam@defn.sh>
2017-09-19 17:10:46 -0700

    set alias var for live stage

--- fn.tf
+++ fn.tf
@@ -97,6 +97,10 @@ resource "aws_api_gateway_deployment" "live" {
   depends_on  = ["module.live"]
   rest_api_id = "${aws_api_gateway_rest_api.env.id}"
   stage_name  = "live"
+
+  vars {
+    alias = "live"
+  }
 }
 
 resource "aws_api_gateway_method_settings" "live" {
