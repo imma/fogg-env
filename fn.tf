6ad2f1efebec968c5a9647eb050e100db109687b
Defn Nghiem <iam@defn.sh>
2017-09-20 19:39:11 -0700

    ignore when deployment changes

--- fn.tf
+++ fn.tf
@@ -64,6 +64,10 @@ resource "aws_lambda_function" "env" {
   runtime          = "python3.6"
   source_code_hash = "${base64sha256(file("${local.deployment_file}"))}"
   publish          = true
+
+  lifecycle {
+    ignore_changes = ["source_code_hash"]
+  }
 }
 
 module "live_fn" {
@@ -101,7 +105,7 @@ resource "aws_api_gateway_base_path_mapping" "live" {
   api_id      = "${aws_api_gateway_rest_api.env.id}"
   stage_name  = "live"
   domain_name = "${signum(length(var.env_zone)) == 1 ? var.env_zone : var.env_name}.${signum(length(var.env_domain_name)) == 1 ? var.env_domain_name : data.terraform_remote_state.org.domain_name}"
-  base_path = "live"
+  base_path   = "live"
 }
 
 resource "aws_api_gateway_method_settings" "live" {
@@ -131,7 +135,7 @@ resource "aws_api_gateway_base_path_mapping" "rc" {
   api_id      = "${aws_api_gateway_rest_api.env.id}"
   stage_name  = "rc"
   domain_name = "${signum(length(var.env_zone)) == 1 ? var.env_zone : var.env_name}.${signum(length(var.env_domain_name)) == 1 ? var.env_domain_name : data.terraform_remote_state.org.domain_name}"
-  base_path = "rc"
+  base_path   = "rc"
 }
 
 resource "aws_api_gateway_method_settings" "rc" {
