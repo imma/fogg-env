0e7fc175316e5b19c7037bbd1039dce5cdb0a01e
Defn Nghiem <iam@defn.sh>
2017-09-20 19:51:16 -0700

    nat is just for http

--- nat.tf
+++ nat.tf
@@ -13,51 +13,6 @@ module "nat" {
   want_eip        = "${var.want_nat_eip}"
 }
 
-resource "aws_security_group_rule" "forward_allow_ping" {
-  type                     = "ingress"
-  from_port                = 8
-  to_port                  = 0
-  protocol                 = "icmp"
-  source_security_group_id = "${aws_security_group.env.id}"
-  security_group_id        = "${module.nat.network_sg}"
-}
-
-resource "aws_security_group_rule" "forward_allow_ssh" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.env.id}"
-  security_group_id        = "${module.nat.network_sg}"
-}
-
-resource "aws_security_group_rule" "forward_allow_ntp" {
-  type                     = "ingress"
-  from_port                = 123
-  to_port                  = 123
-  protocol                 = "udp"
-  source_security_group_id = "${aws_security_group.env.id}"
-  security_group_id        = "${module.nat.network_sg}"
-}
-
-resource "aws_security_group_rule" "forward_allow_dns_udp" {
-  type                     = "ingress"
-  from_port                = 53
-  to_port                  = 53
-  protocol                 = "udp"
-  source_security_group_id = "${aws_security_group.env.id}"
-  security_group_id        = "${module.nat.network_sg}"
-}
-
-resource "aws_security_group_rule" "forward_allow_dns_tcp" {
-  type                     = "ingress"
-  from_port                = 53
-  to_port                  = 53
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.env.id}"
-  security_group_id        = "${module.nat.network_sg}"
-}
-
 resource "aws_security_group_rule" "forward_allow_http" {
   type                     = "ingress"
   from_port                = 80
@@ -71,15 +26,6 @@ resource "aws_security_group_rule" "forward_allow_https" {
   type                     = "ingress"
   from_port                = 443
   to_port                  = 443
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.env.id}"
-  security_group_id        = "${module.nat.network_sg}"
-}
-
-resource "aws_security_group_rule" "forward_irc" {
-  type                     = "ingress"
-  from_port                = 6667
-  to_port                  = 6667
   protocol                 = "tcp"
   source_security_group_id = "${aws_security_group.env.id}"
   security_group_id        = "${module.nat.network_sg}"
