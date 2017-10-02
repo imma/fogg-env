f403013f2e62932c20c92900a672a371440338ef
Defn Nghiem <iam@defn.sh>
2017-10-02 10:57:42 -0700

    fix path to deployment.zip

--- fn.tf
+++ fn.tf
@@ -53,7 +53,7 @@ resource "aws_route53_record" "env_api_gateway_private" {
 
 locals {
   deployment_zip  = ["${split("/","${path.module}/dist/deployment.zip")}"]
-  deployment_file = "${join("/",slice(local.deployment_zip,length(local.deployment_zip)-4,length(local.deployment_zip)))}"
+  deployment_file = "${join("/",slice(local.deployment_zip,length(local.deployment_zip)-5,length(local.deployment_zip)))}"
 }
 
 resource "aws_lambda_function" "env" {
